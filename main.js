a6e8c421c577539f66688f50313020318a862619
Nik0Sp <108077147+Nik0Sp@users.noreply.github.com>
2023-09-02 15:52:55 +0200

    small update

---
+++ main.js
@@ -0,0 +1,11 @@
+let serviceSwiper = new Swiper(".header-container", {
+    slidesPerView: 1,
+    spaceBetween: 500,
+    pagination: {
+        el: ".custom-pagination",
+        clickable: true,
+      
+    },
+    loop: true,
+
+});
